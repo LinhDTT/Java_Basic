345aff548e31b2852be709f7e032fb528399afe6
Linh Dang <linh.dang@episerver.com>
2022-12-01 10:15:05 +0700

    Push Java Code

---
+++ src/DataType.java
@@ -0,0 +1,35 @@
+public class DataType {
+    public static void main(String[] args) {
+        //char khai bao 1 ky tu
+//        char myChar = 'a';
+//        boolean myBoolean = false;
+//        float myFloat = 4.5f;
+//        double myDouble = 4.5;
+////        String studentName[] = new String[]{"Linh", "Hue", "Ngan"};
+//        String myName = "Dang Thuy Linh";
+//        System.out.println(myName);
+//        int x = 50;
+//        int y = 100;
+//        System.out.println(x + y);
+//        System.out.println(x - y);
+//        System.out.println(x * y);
+//        System.out.println(y / x);
+
+//        System.out.println(getString());
+        plusInteger(10, 20);
+    }
+//    public static String getString(){
+//        return "Linh";
+//    }
+
+    //Ham ko co kieu tra ve - void
+    public static void printString() {
+
+        System.out.println("My name is Binh Minh");
+    }
+
+    public static void plusInteger(int x, int y) {
+
+        System.out.println(x + y);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    /**
+     * Hello this for demo comment
+     * @param args
+     */
+    public static void main(String[] args) {
+        // camelCase
+        int myAge = 29;
+
+        // Passcal Case
+        int MyAge = 29;
+
+        // snake_cake
+        int my_age = 29;
+
+        // kebab-case
+//        int long-url= 29;
+        int age = 20;
+        System.out.println(age);
+    }
+}
